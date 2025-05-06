2b606463802b2d2edcb3eac4e8bad7041f20f19e
mahmod alshraky <93828510+mshraky3@users.noreply.github.com>
2025-05-07 02:51:47 +0300

    Add files via upload

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -7,6 +7,8 @@ import Login from '../components/Login/Login.jsx';
 import Register from '../components/Login/register.jsx';
 import Profile from '../components/profile/profile.jsx';
 import Workers from '../components/Workers/Workers.jsx';
+import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from "@vercel/analytics/react"
 
 
 
@@ -39,6 +41,8 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')).render(
     <React.StrictMode>
+        <Analytics />
+        <SpeedInsights />
         <RouterProvider router={router} />
     </React.StrictMode>
 );
